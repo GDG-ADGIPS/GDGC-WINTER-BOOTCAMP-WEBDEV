d37648f67d2f46c0a1e08e2d094361204b7a0d66
rahulkgupta1 <rahulkgupta47@gmail.com>
2025-01-14 01:56:40 +0530

    thankyou

---
+++ Portfolio/js/custom.js
@@ -0,0 +1,14 @@
+var typeData = new Typed(".role", {
+    strings: [
+      "Coder",
+      "Software Developer",
+      "Full Stack Developer",
+      "Web Developer",
+      "UI-UX Designer",
+      "Backend Developer",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
